69868af4545da26ca731c65cd52936e14a78f684
ikoba <i.koba.jp@gmail.com>
2022-10-04 16:52:28 +0900

    add ContainerSample component

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Hello from './components/Hello';
+// import Hello from './components/Hello';
+import ContainerSample from './components/ContainerSample';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Hello />
+    <ContainerSample />
   </React.StrictMode>
 );
 
